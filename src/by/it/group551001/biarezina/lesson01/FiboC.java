a32a5c62e2924917beb07875ac7d622db2e50553
Sofi Biarezina <sofibiarezina@example.com>
2026-02-15 21:24:51 +0300

    complete the 3 task

--- src/by/it/group551001/biarezina/lesson01/FiboC.java
+++ src/by/it/group551001/biarezina/lesson01/FiboC.java
@@ -22,9 +22,38 @@ public class FiboC {
     }
 
     long fasterC(long n, int m) {
+
+        boolean changed = false;
+        long a = 0, c, b = 1;
+
+        long  pred = 0, curr = 1, period = 0;
+        for (int i = 0; i < 6 * m; i++){
+            long temp = curr;
+            curr = (pred + temp) % m;
+            pred = temp;
+
+            if (pred == 0 && curr == 1){
+                period = i + 1;
+                break;
+            }
+        }
+
+        long red_n = (n % period);
+
+        if (n == 0) return 0L;
+        if (n == 1) return 1L;
+        for (int i = 2; i <= (int) red_n + 1; i++){
+            c = b;
+            b = (c + a) % m;
+            a = c;
+
+        }
+
+        return a;
+
         //Интуитивно найти решение не всегда просто и
         //возможно потребуется дополнительный поиск информации
-        return -1L;
+
     }
 
 
